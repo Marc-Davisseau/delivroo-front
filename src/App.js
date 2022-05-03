b8c2bfea91376e1ebe965ac21a97dbb0e13ed71e
Marc <davisseaumarc@gmail.com>
2022-05-03 05:17:25 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3200/");
+     console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+
+
+
+<div className="App1">
+
+
+<div className="logo">
+{/* <h1>logo</h1> */}
+<img src="P10-Deliveroo/Delivroo-Frontend/delivroofront/src/img/deliveroologo.jpg" alt="fdvf" />
+</div>
+
+
+<header>
+       <div className="header1">
+       <h2>{data.restaurant.name}</h2>
+       <p>
+       {data.restaurant.description}
+       </p>       
+       </div>
+       <div className="header2">
+       <img src={data.restaurant.picture} alt="" />
+       </div> 
+</header>
+
+
+<main className="App">
+
+
+{data.categories.map(item =>(
+<div className="main3">
+              <div className="main2">
+       <div className="top">{item.name}</div>
+              {item.meals.map(item =>(
+          <div className="fiche">
+                     <div className="text">
+                <p className="p1">{item.title}</p>
+                <p className="p2">{item.description}</p>
+                <p className="p3">{item.price} €</p>
+                      </div>
+                     <div className="picture">
+                 <img src={item.picture} alt="" />
+                    </div>
+          </div>   
+            ))}
+            </div><div className="side">test</div>
+          </div>
+))}
+
+
+
+</main>
+
+</div>      
+)
+}    
 
 export default App;
